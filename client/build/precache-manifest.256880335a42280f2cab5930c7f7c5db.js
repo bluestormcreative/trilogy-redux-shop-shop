9a103bbadb1a6b6cadd5d2acb25e760a24bcf829
Max MacMillan <shannon@bluestormcreative.com>
2021-04-13 22:00:04 -0700

    Build project

---
+++ client/build/precache-manifest.256880335a42280f2cab5930c7f7c5db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cb45c3434c771c0c35252604f1639d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cef80f6f8e97318335ae",
+    "url": "/static/css/main.e7186c00.chunk.css"
+  },
+  {
+    "revision": "b179262ed1218bfbe0df",
+    "url": "/static/js/2.df12b281.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.df12b281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef80f6f8e97318335ae",
+    "url": "/static/js/main.2cb0a807.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
