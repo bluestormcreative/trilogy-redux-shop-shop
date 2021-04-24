1b8deca95fd8e51b00d46650be6dd01940b05a75
Max MacMillan <max@bluestormcreative.com>
2021-04-23 21:14:39 -0700

    add initialstate to reducer

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -12,7 +12,7 @@ import {
   TOGGLE_CART
 } from "./actions";
 
-export const reducer = (state, action) => {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
     case UPDATE_PRODUCTS:
