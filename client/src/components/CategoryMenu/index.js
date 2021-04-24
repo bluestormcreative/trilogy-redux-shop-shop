8e091919cbf5261ef6e0ae07ced44534039f265b
Max MacMillan <max@bluestormcreative.com>
2021-04-24 13:39:56 -0700

    Update category menu to use redux hooks

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,30 +1,32 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useQuery } from '@apollo/react-hooks';
 
 import { updateCurrentCategory, updateCategories } from '../../utils/actionCreators';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
-function CategoryMenu({categories, setCurrentCategory, setCategories}) {  
+function CategoryMenu() {
+  const categories = useSelector(state => state.categories);
+  const dispatch = useDispatch();
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   const handleClick = (id) => {
-    setCurrentCategory(id);
+    dispatch(updateCurrentCategory(id));
   };
 
   useEffect(() => {
     if (categoryData) {
-      setCategories(categoryData.categories);
+      dispatch(updateCategories(categoryData.categories));
       categoryData.categories.forEach(category => {
         idbPromise('categories', 'put', category);
       });
     } else if (!loading) {
       idbPromise('categories', 'get').then(categories => {
-        setCategories(categories);
+        dispatch(updateCategories(categoryData.categories));
       });
     }
-  }, [categoryData, loading, setCategories]);
+  }, [categoryData, loading, updateCategories]);
 
   return (
     <div>
@@ -43,15 +45,16 @@ function CategoryMenu({categories, setCurrentCategory, setCategories}) {
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    categories: state.categories
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     categories: state.categories
+//   };
+// };
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentCategory: (id) => dispatch(updateCurrentCategory(id)),
-  setCategories: (data) => dispatch(updateCategories(data))
-});
+// const mapDispatchToProps = dispatch => ({
+//   setCurrentCategory: (id) => dispatch(updateCurrentCategory(id)),
+//   setCategories: (data) => dispatch(updateCategories(data))
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CategoryMenu);
+// export default connect(mapStateToProps, mapDispatchToProps)(CategoryMenu);
+export default CategoryMenu;
