b1efea1ea9ee0e9708800a0f01ef07756991af98
Max MacMillan <max@bluestormcreative.com>
2021-04-24 12:39:48 -0700

    Update productitem to pull from redux

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,40 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import {
+  addToCart,
+  updateCartQuantity,
+} from '../../utils/actionCreators';
 import { pluralize, idbPromise } from "../../utils/helpers"
 
-function ProductItem({ item, cart }) {
-
-  const addToCart = () => {};
-  // const addToCart = () => {
-  //   const itemInCart = cart.find((cartItem) => cartItem._id === _id)
-  //   if (itemInCart) {
-  //     dispatch({
-  //       type: UPDATE_CART_QUANTITY,
-  //       _id: _id,
-  //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-  //     });
-  //     idbPromise('cart', 'put', {
-  //       ...itemInCart,
-  //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-  //     });
-  //   } else {
-  //     dispatch({
-  //       type: ADD_TO_CART,
-  //       product: { ...item, purchaseQuantity: 1 }
-  //     });
-  //     idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-  //   }
-  // }
-
+const ProductItem = (props) => {
   const {
+    cart,
+    updateProductCartQuantity,
+    addProductToCart,
     image,
     name,
     _id,
     price,
     quantity
-  } = item;
+  } = props;
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+
+    if (itemInCart) {
+        updateProductCartQuantity({
+          _id: _id,
+          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+        });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+        addProductToCart({ _id, image, name, price, purchaseQuantity: 1 });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { _id, image, name, price, purchaseQuantity: 1 });
+    }
+  };
 
   return (
     <div className="card px-1 py-1">
@@ -52,6 +56,20 @@ function ProductItem({ item, cart }) {
       <button onClick={addToCart}>Add to cart</button>
     </div>
   );
-}
+};
 
-export default ProductItem;
+const mapStateToProps = state => {
+  return {
+    cart: state.cart,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  addProductToCart: (data) => dispatch(addToCart(data)),
+  updateProductCartQuantity: (data) => dispatch(updateCartQuantity(data)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProductItem);
